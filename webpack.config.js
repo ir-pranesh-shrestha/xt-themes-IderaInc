f2b69a4e6998bbaaa211a45fc04d306124115226
vic.d <vic.d0991@gmail.com>
2024-11-04 14:18:48 +0530

    Added nonce attribute in Style Element before it's inserted

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,12 @@ let debServerConfig = {
       loader: 'babel-loader'
     }, {
       test:/\.css$/,
-      use:[ devMode ? 'style-loader': MiniCssExtractPlugin.loader,'css-loader']
+      use:[ {
+        loader: "style-loader",
+        options: {
+          insert: require.resolve("./insert-function"),
+        },
+      },'css-loader']
     }]
   };
 
